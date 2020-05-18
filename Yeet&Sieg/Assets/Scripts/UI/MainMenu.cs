10cbdf6d994a2af4c22780b0b58015eb19d04d57
Zoey <prof.natasaki@gmail.com>
2020-05-18 15:51:49 +0200

    Basic Menu Preperations

---
+++ Yeet&Sieg/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadSceneAsync("GameScene");
+    }
+
+    public void OpenOptions()
+    {
+        //Open the Options Screen
+    }
+
+    public void OpenScoreboard()
+    {
+        //Fetch all Score Data
+        //Open the Scoreboard Screen
+    }
+
+    public void Skins()
+    {
+        //Open Skins Screen
+    }
+}
