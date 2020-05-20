fc2e2fad41ab9036ff6aea83390bd1c90d5aa8a9
Zoey <prof.natasaki@gmail.com>
2020-05-20 10:55:39 +0200

    Pause Menu implemented

---
+++ Yeet&Sieg/Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    PauseMenuController menu;
+
+    private void Start()
+    {
+        menu = FindObjectOfType<PauseMenuController>();
+    }
+
+    public void ResumeButton()
+    {
+        menu.ResumeGame();
+    }
+
+    public void OptionsButton()
+    {
+        menu.OpenOptions();
+    }
+
+    public void RestartButton()
+    {
+        menu.RestartGame();
+    }
+
+    public void BackToMenuButton()
+    {
+        menu.BackToMenu();
+    }
+}
