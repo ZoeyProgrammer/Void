c763024297dcb1638aee0446d4d906ffe9c8845b
Zoey <prof.natasaki@gmail.com>
2020-06-16 15:25:35 +0200

    Renamed to avoid self-injection

---
+++ YeetIt/Assets/Scripts/FirebaseInit.cs
@@ -0,0 +1,15 @@
+﻿using Firebase;
+using Firebase.Analytics;
+using UnityEngine;
+
+public class FirebaseInit : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Firebase Init");
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+        {
+            FirebaseAnalytics.SetAnalyticsCollectionEnabled(true);
+        });
+    }
+}
