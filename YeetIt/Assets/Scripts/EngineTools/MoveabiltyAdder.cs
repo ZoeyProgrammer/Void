dbc1ef878d58f805c55ba1b7288d52f1e52b5cfe
Zoey <prof.natasaki@gmail.com>
2020-06-30 16:24:00 +0200

    Moving Objects added
    Just add the MovabiltyMarker to every prefab where it's needed, I did not bother to do it to every single one

---
+++ YeetIt/Assets/Scripts/EngineTools/MoveabiltyAdder.cs
@@ -0,0 +1,29 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(MoveabiltyMark))]
+public class MoveabiltyAdder : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        MoveabiltyMark obj = (MoveabiltyMark)target;
+
+
+        if (obj.GetComponent<Moveability>() == null)
+        {
+            if (GUILayout.Button("Make Moving Object"))
+            {
+                obj.gameObject.AddComponent(typeof(Moveability));
+                obj.gameObject.AddComponent(typeof(Rigidbody2D));
+                GameObject keyframe = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefabs/Technical/Keyframe.prefab");
+                GameObject key = Instantiate(keyframe, obj.transform.position, Quaternion.identity);
+
+                obj.GetComponent<Moveability>().keyframes.Add(key.transform);
+            }
+        }
+        else
+            GUILayout.Label("My work here is done");
+    }
+}
+#endif
---
+++ YeetIt/Assets/Scripts/Interactables/Moveability.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moveability : MonoBehaviour
+{
+    public List<Transform> keyframes = new List<Transform>();
+    public float speed = 10;
+
+    public int currentKeyframe = 0;
+    private Transform currentTarget;
+    private float distance;
+    private float degrees;
+
+    void Update()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        if (transform.position == keyframes[currentKeyframe].position)
+        {
+            if (currentKeyframe < keyframes.Count - 1)
+                currentKeyframe++;
+            else
+                currentKeyframe = 0;
+
+            currentTarget = keyframes[currentKeyframe];
+            distance = Vector3.Distance(transform.position, currentTarget.position);
+            degrees = transform.rotation.eulerAngles.z - currentTarget.rotation.eulerAngles.z;
+            Debug.Log(transform.rotation.eulerAngles.z + " - " + currentTarget.rotation.eulerAngles.z + " = " + degrees);
+        }
+        transform.position = Vector3.MoveTowards(transform.position, currentTarget.position, Time.deltaTime * speed);
+        float rotationSpeed = degrees / (distance / speed);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, currentTarget.rotation, Time.deltaTime * Mathf.Abs(rotationSpeed));
+    }
+}
